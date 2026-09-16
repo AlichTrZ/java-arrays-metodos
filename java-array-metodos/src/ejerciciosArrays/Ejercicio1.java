cb928f40dabcc6f9b5fa855b6b4629af4399ea22
AlichTrZ <felipetro44@gmail.com>
2026-09-15 19:07:38 -0500

    Establezco el repositorio.

---
+++ java-array-metodos/src/ejerciciosArrays/Ejercicio1.java
@@ -0,0 +1,19 @@
+/*
+Si tenemos de inicio un array int[] miarray = {8,2,5,4,9,1,0,8,9,3}
+construir un programa que copie miarray en otro array llamado copiaDeDatos, pero sumando 2 a cada
+elemento, y que escriba el array copiaDeDatos por pantalla, esto es, que salga : "el elemento 1 de copiadedatos es 10”
+
+"el elemento 2 de copiadedatos es 4"
+"el elemento 3 de copiadedatos es 7"
+"el elemento 4 de copiadedatos es 6"
+...etc...
+ */
+package ejerciciosArrays;
+
+/**
+ *
+ * @author Andres Felipe Trochez Lugo
+ */
+public class Ejercicio1 {
+    
+}
